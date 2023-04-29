ba52c7b53846e244aab67df7055011b55736cd45
v1p3rrr <cleverminer@gmail.com>
2023-04-29 14:09:25 +0300

    changed dependencies, fixed build problems

--- app/src/main/java/com/vpr/gasoline_prices_app/ui/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/vpr/gasoline_prices_app/ui/splash_screen/SplashScreen.kt
@@ -1,10 +1,17 @@
 package com.vpr.gasoline_prices_app.ui.splash_screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun SplashScreen(
 
 ){
 
+}
+
+@Preview
+@Composable
+fun PreviewSplashScreen(){
+    SplashScreen()
 }
