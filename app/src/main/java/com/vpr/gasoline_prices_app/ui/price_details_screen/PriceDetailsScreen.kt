1f05fe9ad0e3c0b3808c1b141caae4e08a4de1a8
Andrey <70503655+SiriHate@users.noreply.github.com>
2023-04-30 13:32:22 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/v1p3rrr/gasoline-prices-app""
    This reverts commit 41da50e22f2acf6f6bed648a7de05a1523066181.

--- app/src/main/java/com/vpr/gasoline_prices_app/ui/price_details_screen/PriceDetailsScreen.kt
+++ app/src/main/java/com/vpr/gasoline_prices_app/ui/price_details_screen/PriceDetailsScreen.kt
@@ -12,17 +12,20 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PriceDetailsScreen(navController: NavController, snackBarHostState: SnackbarHostState) {
+    val viewModel: PriceDetailsViewModel = viewModel()
+    val priceDetailsState = viewModel.priceDetailsState.value
+
     Scaffold(
         snackbarHost = { SnackbarHost(snackBarHostState) },
         content = { padding ->
@@ -30,28 +33,30 @@ fun PriceDetailsScreen(navController: NavController, snackBarHostState: Snackbar
             Column() {
                 Row(Modifier.fillMaxWidth().padding(vertical = 16.dp)) {
                     Text(modifier = Modifier.width(120.dp).padding(start = 16.dp), text = "")
-                    Text(
-                        modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
-                        textAlign = TextAlign.Center,
-                        text = "15.07.2021"
-                    )
-                    Text(
-                        modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
-                        textAlign = TextAlign.Center,
-                        text = "15.12.2021"
-                    )
+                        Text(
+                            modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
+                            textAlign = TextAlign.Center,
+                            text = priceDetailsState.gasolinePriceListChosen[0].date
+                        )
+                        Text(
+                            modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
+                            textAlign = TextAlign.Center,
+                            text = priceDetailsState.gasolinePriceListToday[0].date
+                        )
+                    }
                 }
                 Row(Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
                     Text(modifier = Modifier.width(120.dp).padding(start = 16.dp), text = "А-92")
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.00 руб."
+                        text = priceDetailsState.gasolinePriceListChosen.find { it.gasolineType == "92" }?.price?.toString() ?: "—"
                     )
+                    val a = priceDetailsState.gasolinePriceListChosen.find { it.gasolineType == "92" }?.price
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.50 руб."
+                        text = priceDetailsState.gasolinePriceListToday.find { it.gasolineType == "92" }?.price?.toString() ?: "—"
                     )
                 }
                 Row(Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
@@ -59,12 +64,12 @@ fun PriceDetailsScreen(navController: NavController, snackBarHostState: Snackbar
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "11.00 руб."
+                        text = priceDetailsState.gasolinePriceListChosen.find { it.gasolineType == "95" }?.price?.toString() ?: "—"
                     )
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "11.50 руб."
+                        text = priceDetailsState.gasolinePriceListToday.find { it.gasolineType == "95" }?.price?.toString() ?: "—"
                     )
                 }
                 Row(Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
@@ -72,25 +77,12 @@ fun PriceDetailsScreen(navController: NavController, snackBarHostState: Snackbar
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.00 руб."
+                        text = priceDetailsState.gasolinePriceListChosen.find { it.gasolineType == "98" }?.price?.toString() ?: "—"
                     )
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.50 руб."
-                    )
-                }
-                Row(Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
-                    Text(modifier = Modifier.width(120.dp).padding(start = 16.dp), text = "А-100")
-                    Text(
-                        modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
-                        textAlign = TextAlign.Center,
-                        text = "10.00 руб."
-                    )
-                    Text(
-                        modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
-                        textAlign = TextAlign.Center,
-                        text = "10.50 руб."
+                        text = priceDetailsState.gasolinePriceListToday.find { it.gasolineType == "98" }?.price?.toString() ?: "—"
                     )
                 }
                 Row(Modifier.fillMaxWidth().padding(vertical = 8.dp)) {
@@ -98,17 +90,17 @@ fun PriceDetailsScreen(navController: NavController, snackBarHostState: Snackbar
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.00 руб."
+                        text = priceDetailsState.gasolinePriceListChosen.find { it.gasolineType == "diesel" }?.price?.toString() ?: "—"
                     )
                     Text(
                         modifier = Modifier.weight(1f).padding(horizontal = 8.dp),
                         textAlign = TextAlign.Center,
-                        text = "10.50 руб."
+                        text = priceDetailsState.gasolinePriceListToday.find { it.gasolineType == "diesel" }?.price?.toString() ?: "—"
                     )
                 }
             }
         }
-    })
+    )
 }
 
 @Preview
